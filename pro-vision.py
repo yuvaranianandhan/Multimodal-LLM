f6088e6fdf1b64a4796a3d3ad097b5659253e570
Yuvarani Anandhan <111055786+yuvaranianandhan@users.noreply.github.com>
2024-02-26 22:03:06 +0530

    Update pro-vision.py

--- pro-vision.py
+++ pro-vision.py
@@ -28,7 +28,7 @@ def get_response(input,image):
     else:
         if image is not None:
             response = model.generate_content(image)
-            return response.parts
+            return response.text
         else:
             response = "Please provide either an input prompt or upload an image."
             return response.text
@@ -45,11 +45,14 @@ input = st.text_input("Input Prompt", key="input")
 
 uploaded_file = st.file_uploader("Input Image", type=["jpg", "jpeg", "png"])
 
-image=None  
+image=""  
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
+else:
+    image = None
+    
 
 
 submit = st.button("submit")
